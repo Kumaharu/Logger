a03eb38eb32b3ef8e529d606dffd1286f51ecf6b
Tomoya Uchihara <e165728@ie.u-ryukyu.ac.jp>
2018-10-09 14:46:54 +0900

    Change Gemfile

--- app/controllers/input_text_controller.rb
+++ app/controllers/input_text_controller.rb
@@ -1,21 +1,30 @@
 class InputTextController < ApplicationController
+  before_action :set_spot, only: [:show]
 
   def index
     @spot = Spot.all
   end
 
   def show
-    @spot = Spot.find(params[:id])
   end
 
   def new
   end
 
-  def create 
+  def create
     @spot = Spot.new
-    @spot.visit  = params[:spot][:visit]
+    @spot.visit = params[:spot][:visit]
     @spot.comment = params[:spot][:comment]
     @spot.save
     redirect_to '/'
+  end
+
+  private
+  def plan_params
+    params.require(:spot).permit(:visit, :comment)
+  end
+
+  def set_spot
+    @spot = Spot.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   root 'input_text#index'
-  resources :input_text
+  post 'input_text', to: 'input_text#create'
+  get 'input_text', to: 'input_text#index'
+  get 'input_text/new', to: 'input_text#new', as: 'new_input'
+  get 'input_text/index', to: 'input_text#index', as: 'index_input'
+  get 'input_text/:id', to: 'input_text#show', as: 'show_input'
 end
