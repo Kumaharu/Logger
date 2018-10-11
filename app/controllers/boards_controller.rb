0badb5eaed3c90bb08a5da2118d876ecce2e94a1
kuma <e165720@ie.u-ryukyu.ac.jp>
2018-10-11 16:44:59 +0900

    ボードを作成できた

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -4,7 +4,15 @@ class BoardsController < ApplicationController
     @board = Board.all
   end
 
+  def create
+    @board = Board.new(params[:board].permit(:title))
+    @board.save
+
+    redirect_to boards_index_path    
+  end
+
   def new_board
+    @board = Board.new
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   root "boards#index"
   get "boards/new_board", to: "boards#new_board"
+  get "boards/index",     to: "boards#index"
+  post "boards/create", to: "boards#create"
 end
