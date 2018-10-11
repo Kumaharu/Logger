b8e4dbfb48985f5619ef1510db0fa773a20ae193
kuma <e165720@ie.u-ryukyu.ac.jp>
2018-10-11 15:29:30 +0900

    Create controller, homepage and new_board

---
+++ app/controllers/boards_controller.rb
@@ -0,0 +1,6 @@
+class BoardsController < ApplicationController
+  def index
+  end
+  def new_board
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
-  root 'input_text#index'
-  post 'input_text', to: 'input_text#create'
-  get 'input_text', to: 'input_text#index'
-  get 'input_text/new', to: 'input_text#new', as: 'new_input'
-  get 'input_text/index', to: 'input_text#index', as: 'index_input'
-  get 'input_text/:id', to: 'input_text#show', as: 'show_input'
+  root "boards#index"
+  get "boards/new_board", to: "boards#new_board"
 end
